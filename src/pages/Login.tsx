689646d8983a8f872c69feea5604d0ea5d3c5bb1
GandoPlay <edo22061@gmail.com>
2023-09-17 14:34:02 +0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,10 +10,21 @@ import {
 import { useState } from "react";
 import { NumberInput, NumberInputField } from "@chakra-ui/react";
 import crypto from "crypto";
+import { useForm } from "react-hook-form";
+import { env } from "~/env.mjs";
 
 function App() {
-  const [showInput, setshowInput] = useState(false);
-  const [didVote, setDidVote] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => {
+    if (data.password === env.PASSWORD) {
+      localStorage.setItem("adminID", data.password);
+    }
+  };
 
   const breatheAnimation = keyframes`
     0% { transform: scale(0.9); }
@@ -22,26 +33,32 @@ function App() {
     100% { transform: scale(0.9); }
   `;
   return (
-    <Box w="100vw" h="100vh" bgGradient="linear(to-r, blue.500, blue.200)">
-      <Center h="100vh">
-        <Flex dir="rtl" flexDir={"column"} gap={3}>
-          <Text fontSize={"xl"}>התחברות</Text>
-          <Input type={"password"} placeholder="סיסמא"></Input>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box w="100vw" h="100vh" bgGradient="linear(to-r, blue.500, blue.200)">
+        <Center h="100vh">
+          <Flex dir="rtl" flexDir={"column"} gap={3}>
+            <Text fontSize={"xl"}>התחברות</Text>
+            <Input
+              {...register("password", { required: true })}
+              type={"password"}
+              placeholder="סיסמא"
+            ></Input>
 
-          <Button
-            onClick={() => {
-              if (!localStorage.getItem("sessionId")) {
-                const sessionId = crypto.randomBytes(64).toString("hex");
+            <Button
+              onClick={() => {
+                if (!localStorage.getItem("sessionId")) {
+                  const sessionId = crypto.randomBytes(64).toString("hex");
 
-                localStorage.setItem("sessionId", sessionId);
-              }
-            }}
-          >
-            שלח
-          </Button>
-        </Flex>
-      </Center>
-    </Box>
+                  localStorage.setItem("sessionId", sessionId);
+                }
+              }}
+            >
+              שלח
+            </Button>
+          </Flex>
+        </Center>
+      </Box>
+    </form>
   );
 }
 
